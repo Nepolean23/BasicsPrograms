5e6ef5ac0521cbb692c43fa62352be151c64d83e
Nepolean23 <nlean1996@gmail.com>
2022-12-04 15:13:00 +0530

    Code For PrimeFactor

---
+++ BasicProgram/BasicProgram/PrimeFactor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class PrimeFactor
+    {
+        public static void PrimeFactorNumber()
+        {
+            Console.WriteLine("Enter a number to find prime number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int divisor = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    divisor++;
+                }
+            }
+            if (divisor == 2)
+            {
+                Console.WriteLine("It is a prime number:" + number);
+            }
+            else
+            {
+                Console.WriteLine("It is not a prime number:" + number);
+            }
+        }
+    }
+}
--- BasicProgram/BasicProgram/Program.cs
+++ BasicProgram/BasicProgram/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Basic Programs");
 while (true)
 {   
     Console.WriteLine("Enter the option to perform the operation \n1) FlipCoin \n2) LeapYear \n3) HarmonicSeries "+
-        "\n4)PowerOfTwo");
+        "\n4)PowerOfTwo\n5)PrimeFactor");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -21,6 +21,9 @@ while (true)
             break;
         case 4:
             PowerOfTwo.PowerOfBase(); 
+            break;
+        case 5:
+            PrimeFactor.PrimeFactorNumber();
             break;
         
              
