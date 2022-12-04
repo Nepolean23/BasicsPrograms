34b6c9caaca2c727ff35beaec46663a8c588636b
Nepolean23 <nlean1996@gmail.com>
2022-12-04 14:58:12 +0530

    HarmonicSeries

---
+++ BasicProgram/BasicProgram/HarmonicSeries.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class HarmonicSeries
+    {
+        public static void Harmonic()
+        {
+            Console.WriteLine("Enter a value to find nth harmonic series");
+            int H = Convert.ToInt32(Console.ReadLine());
+            if (H != 0)
+            {
+
+                for (int i = 1; i <= H; i++)
+                {
+
+                    Console.Write("1/{0}+", i);
+
+                }
+            }
+
+            else
+
+            {
+                Console.WriteLine("you have entered in valied value");
+            }
+            
+        }
+    }
+
+}
+  
--- BasicProgram/BasicProgram/Program.cs
+++ BasicProgram/BasicProgram/Program.cs
@@ -5,7 +5,8 @@ using BasicProgram;
 Console.WriteLine("Basic Programs");
 while (true)
 {   
-    Console.WriteLine("Enter the option to perform the operation \n1) FlipCoin \n2) LeapYear ");
+    Console.WriteLine("Enter the option to perform the operation \n1) FlipCoin \n2) LeapYear \n3) HarmonicSeries "+
+        "\n4)PowerOfTwo");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -15,6 +16,16 @@ while (true)
         case 2:
             LeapYear.CheckingLeapYear();
             break;
+        case 3:
+            HarmonicSeries.Harmonic();
+            break;
+        
+             
+            
+        
+
+
+
        
     }
 }
