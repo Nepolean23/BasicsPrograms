ded2a0c159bbaf73ac475eed41b40aa947962e09
Nepolean23 <nlean1996@gmail.com>
2022-12-04 15:37:10 +0530

    Code For SwapingTwoNumbers

--- BasicProgram/BasicProgram/Program.cs
+++ BasicProgram/BasicProgram/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Basic Programs");
 while (true)
 {   
     Console.WriteLine("Enter the option to perform the operation \n1) FlipCoin \n2) LeapYear \n3) HarmonicSeries "+
-        "\n4)PowerOfTwo\n5)PrimeFactor\n6)QuotientAndReminder");
+        "\n4)PowerOfTwo\n5)PrimeFactor\n6)QuotientAndReminder\n7)SwapingTwoNumbers");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -27,6 +27,9 @@ while (true)
             break;
         case 6:
             QuotientAndReminder.CheckingQuotientAndReminder();
+            break;
+        case 7:
+            SwapTwoNumbers.SwapingNumbers();
             break;
         
              
---
+++ BasicProgram/BasicProgram/SwapTwoNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class SwapTwoNumbers
+    {
+        public static void SwapingNumbers()
+        {
+            Console.WriteLine("Enter a value for a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" a:" + a + " b:" + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("swaped value:" + "a:" + a + " b:" + b);
+        }
+    }
+}
