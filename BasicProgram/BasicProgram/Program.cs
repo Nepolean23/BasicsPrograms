a03de36f5423a51c2a5746cfdd61f5f3a399499e
Nepolean23 <nlean1996@gmail.com>
2022-12-04 15:31:47 +0530

    Code for QuotientAndReminder

--- BasicProgram/BasicProgram/Program.cs
+++ BasicProgram/BasicProgram/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Basic Programs");
 while (true)
 {   
     Console.WriteLine("Enter the option to perform the operation \n1) FlipCoin \n2) LeapYear \n3) HarmonicSeries "+
-        "\n4)PowerOfTwo\n5)PrimeFactor");
+        "\n4)PowerOfTwo\n5)PrimeFactor\n6)QuotientAndReminder");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -24,6 +24,9 @@ while (true)
             break;
         case 5:
             PrimeFactor.PrimeFactorNumber();
+            break;
+        case 6:
+            QuotientAndReminder.CheckingQuotientAndReminder();
             break;
         
              
---
+++ BasicProgram/BasicProgram/QuotientAndReminder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class QuotientAndReminder
+    {
+        public static void CheckingQuotientAndReminder()
+        {
+            Console.WriteLine("Enter a value for divident:");
+            int divident = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for divisor:");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            int quotient = divident / divisor;
+            int reminder = divident % divisor;
+            Console.WriteLine("Quotient:" + quotient);
+            Console.WriteLine("Reminder:" + reminder);
+        }
+    }
+}
