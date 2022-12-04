e3e74518f3f22978ad011b12fc7c17e1f23607ea
Nepolean23 <nlean1996@gmail.com>
2022-12-04 16:29:13 +0530

    Code For VowelOrConsonant Program

--- BasicProgram/BasicProgram/Program.cs
+++ BasicProgram/BasicProgram/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Basic Programs");
 while (true)
 {   
     Console.WriteLine("Enter the option to perform the operation \n1) FlipCoin \n2) LeapYear \n3) HarmonicSeries "+
-        "\n4)PowerOfTwo\n5)PrimeFactor\n6)QuotientAndReminder\n7)SwapingTwoNumbers\n8)EvenOROddNumbers");
+        "\n4)PowerOfTwo\n5)PrimeFactor\n6)QuotientAndReminder\n7)SwapingTwoNumbers\n8)EvenOROddNumbers\n9)VowelOrConsonant");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -34,6 +34,10 @@ while (true)
         case 8:
             EvenOrOdd.CheckingEvenOrOdd();
             break;
+        case 9:
+            VowelOrConsonant.CheckingVowelOrConsonant();
+            break;
+
         
              
             
---
+++ BasicProgram/BasicProgram/VowelOrConsonant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class VowelOrConsonant
+    {
+        public static void CheckingVowelOrConsonant()
+        {
+            Console.WriteLine("Enter a character to find a vowel");
+            char name = Convert.ToChar(Console.ReadLine());
+            if (name == 'a' || name == 'e' || name == 'i' || name == 'o' || name == 'u')
+            {
+                Console.WriteLine("The given character:" + name + " is a vowel");
+            }
+            else
+            {
+                Console.WriteLine("The given character:" + name + " is not a vowel");
+            }
+        }
+    }
+}
